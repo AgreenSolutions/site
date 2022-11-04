6e9dc668db33bc819270d359459851f402f48c74
leonardosantosborges <leonardo.borges@sptech.school>
2022-11-04 13:39:54 -0300

    Script Azure - Login e Cadastro funcionando

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -3,10 +3,10 @@ var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
 var sqlServerConfig = {
-    server: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    server: "**",
+    database: "**",
+    user: "**",
+    password: "**",
     pool: {
         max: 10,
         min: 0,
